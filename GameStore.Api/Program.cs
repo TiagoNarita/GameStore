f5bdd7f3d9913ebdc98f35c9dda7bf1772c31dec
Tiago <tiagobnarita@gmail.com>
2024-09-17 00:03:01 -0300

    first commit

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,29 @@
+using GameStore.Api.Dtos;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<GameDto> games =
+[
+    new GameDto(1, "Street Fighter 2", "Fighting", 19.99M, new DateOnly(1992, 7, 15)),
+    new GameDto(2, "Narita", "Roleplaying", 59.99M, new DateOnly(2010, 7, 15)),
+    new GameDto(3, "FIFA 23", "Sports", 63.99M, new DateOnly(2022, 9, 27)),
+];
+
+//GET /games
+app.MapGet("games", () => games);
+
+//GET /games/1
+app.MapGet("games/{id}", (int id)=> games.Find(game =>game.Id == id));
+
+app.MapPost("games", (CreateGameDto newGame) =>{
+    GameDto gameDto= new (
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+});
+
+app.Run();
